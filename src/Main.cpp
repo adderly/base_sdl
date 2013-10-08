848ec465d79225b17d86fbe816e79286ac7f47b8
Aaron Dale <aaron@nullptr.net>
2013-10-08 12:43:01 -0700

    Added main.cpp

---
+++ src/Main.cpp
@@ -0,0 +1,12 @@
+//
+//  Main.cpp
+//  BaseSDL
+//
+//  Created by Aaron Dale on 2013-10-08.
+//  Copyright (c) 2013 Aaron Dale. All rights reserved.
+//
+
+int main(int argc, char** argv)
+{
+    return 0;
+}
