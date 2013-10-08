55afe101f13610cab441876d40f9782fc6f01c36
Aaron Dale <aaron@nullptr.net>
2013-10-08 13:45:02 -0700

    Moved linux libraries into lib foldeR

--- Debug/src/Main.d
+++ Debug/src/Main.d
@@ -1,6 +1,49 @@
 src/Main.d: ../src/Main.cpp ../src/Common.h ../src/Platform.h \
- ../src/Debug.h ../src/OpenGL.h ../src/Singleton.h \
- ../src/SDLWindowManager.h
+ ../src/Debug.h ../src/OpenGL.h \
+ /home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL.h \
+ /home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_main.h \
+ /home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_stdinc.h \
+ /home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_config.h \
+ /home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_platform.h \
+ /home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/begin_code.h \
+ /home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/close_code.h \
+ /home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_assert.h \
+ /home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_atomic.h \
+ /home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_audio.h \
+ /home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_error.h \
+ /home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_endian.h \
+ /home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_mutex.h \
+ /home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_thread.h \
+ /home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_rwops.h \
+ /home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_clipboard.h \
+ /home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_cpuinfo.h \
+ /home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_events.h \
+ /home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_video.h \
+ /home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_pixels.h \
+ /home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_rect.h \
+ /home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_surface.h \
+ /home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_blendmode.h \
+ /home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_keyboard.h \
+ /home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_keycode.h \
+ /home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_scancode.h \
+ /home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_mouse.h \
+ /home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_joystick.h \
+ /home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_gamecontroller.h \
+ /home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_quit.h \
+ /home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_gesture.h \
+ /home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_touch.h \
+ /home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_haptic.h \
+ /home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_hints.h \
+ /home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_loadso.h \
+ /home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_log.h \
+ /home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_messagebox.h \
+ /home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_power.h \
+ /home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_render.h \
+ /home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_system.h \
+ /home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_timer.h \
+ /home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_version.h \
+ /home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_opengl.h \
+ ../src/Singleton.h ../src/SDLWindowManager.h
 
 ../src/Common.h:
 
@@ -9,6 +52,92 @@ src/Main.d: ../src/Main.cpp ../src/Common.h ../src/Platform.h \
 ../src/Debug.h:
 
 ../src/OpenGL.h:
+
+/home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL.h:
+
+/home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_main.h:
+
+/home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_stdinc.h:
+
+/home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_config.h:
+
+/home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_platform.h:
+
+/home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/begin_code.h:
+
+/home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/close_code.h:
+
+/home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_assert.h:
+
+/home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_atomic.h:
+
+/home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_audio.h:
+
+/home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_error.h:
+
+/home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_endian.h:
+
+/home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_mutex.h:
+
+/home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_thread.h:
+
+/home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_rwops.h:
+
+/home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_clipboard.h:
+
+/home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_cpuinfo.h:
+
+/home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_events.h:
+
+/home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_video.h:
+
+/home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_pixels.h:
+
+/home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_rect.h:
+
+/home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_surface.h:
+
+/home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_blendmode.h:
+
+/home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_keyboard.h:
+
+/home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_keycode.h:
+
+/home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_scancode.h:
+
+/home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_mouse.h:
+
+/home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_joystick.h:
+
+/home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_gamecontroller.h:
+
+/home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_quit.h:
+
+/home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_gesture.h:
+
+/home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_touch.h:
+
+/home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_haptic.h:
+
+/home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_hints.h:
+
+/home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_loadso.h:
+
+/home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_log.h:
+
+/home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_messagebox.h:
+
+/home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_power.h:
+
+/home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_render.h:
+
+/home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_system.h:
+
+/home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_timer.h:
+
+/home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_version.h:
+
+/home/aaron/repositories/BaseSDL/lib/sdl2/include/SDL2/SDL_opengl.h:
 
 ../src/Singleton.h:
 
